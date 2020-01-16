4c2b8bf926d411fe3a2e960ac3395cbd31fbf5e0
Olsen <olsencar@oregonstate.edu>
2020-01-16 08:02:44 -0800

    Now using a dataset with more words

--- wordFinder.py
+++ wordFinder.py
@@ -1,31 +1,34 @@
-import nltk
-nltk.download('words')
-from nltk.corpus import words
 from timeit import default_timer as timer
+from itertools import permutations
 
-dictionary = set(words.words())
+MIN_WORD_SIZE = 2
+words = set()
 
-def isEnglishWord(word):
-    return word.lower() in dictionary
+def isWord(word):
+    return word in words
 
-def recurCombination(text, currentString, i, textLen, combLen, wordsFound, wordsFoundIdx): 
-    if combLen > textLen:
-        return
-    if combLen == 0 and isEnglishWord(currentString):
-        wordsFound[wordsFoundIdx].add(currentString)
-        # print("\t", currentString)
-        return
-    
-    for j in range(i, textLen):
-        recurCombination(text, currentString + text[j], j + 1, textLen, combLen - 1, wordsFound, wordsFoundIdx)
+def getAllWords():
+    with open('words.txt', 'r') as f:
+        word = f.readline().rstrip().lower()
+        while word:
+            words.add(word)
+            word = f.readline().rstrip().lower()
+
+def permutation(text, n):
+    return map(lambda x: ''.join(x), permutations(list(text), n))
 
 def main():
-    text = input("Enter the letters: ")
-    wordsFound = [set() for i in range(len(text))]
-    MIN_WORD_SIZE = 2
+    text = input("Enter the letters: ").strip().lower()
+    getAllWords()
+    wordsFound = []
+
     for i in range(MIN_WORD_SIZE, len(text)):
-        recurCombination(text, "", 0, len(text), i, wordsFound, i - MIN_WORD_SIZE)
-        
+        wordsFound.append([])
+        strPerms = set(permutation(text, i))
+        for word in strPerms:
+            if word in words:
+                wordsFound[i - MIN_WORD_SIZE].append(word)
+    
     i = MIN_WORD_SIZE
     for arr in wordsFound:
         if (len(arr) > 0):
